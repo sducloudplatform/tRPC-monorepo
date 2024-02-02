2dd8fde7977ae054479232b1e2f126378f9a7852
Yanning(Ethan) Xue <yanningx@andrew.cmu.edu>
2024-02-02 22:10:00 +0800

    refactor: export common parts from electron & mini-app

---
+++ apps/common/src/api.d.ts
@@ -0,0 +1,28 @@
+export declare const trpc: {
+    hello: {
+        query: import("@trpc/client").Resolver<import("@trpc/server").BuildProcedure<"query", {
+            _config: import("@trpc/server").RootConfig<{
+                ctx: {
+                    token: string;
+                };
+                meta: object;
+                errorShape: import("@trpc/server").DefaultErrorShape;
+                transformer: import("@trpc/server").DefaultDataTransformer;
+            }>;
+            _meta: object;
+            _ctx_out: {
+                token: string;
+            };
+            _input_in: {
+                name?: string;
+            };
+            _input_out: {
+                name?: string;
+            };
+            _output_in: typeof import("@trpc/server").unsetMarker;
+            _output_out: typeof import("@trpc/server").unsetMarker;
+        }, {
+            text: string;
+        }>>;
+    };
+};
--- apps/electron/electron.vite.config.ts
+++ apps/electron/electron.vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src'),
+        '@common': resolve('../../apps/common/src')
       }
     },
     plugins: [vue()]
