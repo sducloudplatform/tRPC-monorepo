9e5f79469810d4c326516572eca0b830b522cfbe
herbenri <2417577995@qq.com>
2024-03-06 12:49:25 +0800

    update:user-ui(1)

--- apps/mini-app/src/pages/user/user.vue
+++ apps/mini-app/src/pages/user/user.vue
@@ -1,19 +1,187 @@
-<template>
-    <div class="user">
-        <h1>user page</h1>
-    </div>
-</template>
+<script setup lang="ts">
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
 
-export default defineComponent({
-    name: 'user',
-});
+// 获取会员信息
+// const memberStore = useMemberStore()
+
+// const { guessRef, onScrolltolower } = useGuessList()
 </script>
 
-<style scoped>
-.user {
-    /* Add your styles here */
+<template>
+  <scroll-view enable-back-to-top @scrolltolower="onScrolltolower" class="viewport" scroll-y>
+    <!-- 个人资料 -->
+    <view class="profile" :style="{ paddingTop: safeAreaInsets!.top + 'px' }">
+      <view class="overview" >
+        <navigator url="/pagesMember/profile/profile" hover-class="none">
+          <image class="avatar" src="../../static/OIP-C.jpg" mode="aspectFill"></image>
+        </navigator>
+        <view class="meta">
+          <view class="nickname">
+            hahah
+            <!-- {{ memberStore.profile.nickname || memberStore.profile.account }} -->
+          </view>
+          <navigator class="extra" url="/pagesMember/profile/profile" hover-class="none">
+            <text class="update">更新头像昵称</text>
+          </navigator>
+        </view>
+      </view>
+
+    </view>
+    <!-- 我的信息 -->
+    <view class="orders">
+      <view class="title">
+        基本信息
+        <!-- <navigator class="navigator" url="/pagesOrder/list/list?type=0" hover-class="none">
+          查看全部订单<text class="icon-right"></text>
+        </navigator> -->
+        <navigator
+          url="/pages/"
+          open-type="navigate"
+          hover-class="navigator-hover"
+        >
+        个人信息
+        
+        </navigator>
+      </view>
+    </view>
+    
+  </scroll-view>
+</template>
+
+<style lang="scss">
+page {
+  height: 100%;
+  overflow: hidden;
+  background-color: #f7f7f8;
+}
+
+.viewport {
+  height: 100%;
+  background-repeat: no-repeat;
+  background-image: url(https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/images/center_bg.png);
+  background-size: 100% auto;
+}
+
+/* 用户信息 */
+.profile {
+  margin-top: 30rpx;
+  position: relative;
+
+  .overview {
+    display: flex;
+    height: 120rpx;
+    padding: 0 36rpx;
+    color: #fff;
+  }
+
+  .avatar {
+    width: 120rpx;
+    height: 120rpx;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+
+  .gray {
+    filter: grayscale(100%);
+  }
+
+  .meta {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    line-height: 30rpx;
+    padding: 16rpx 0;
+    margin-left: 20rpx;
+  }
+
+  .nickname {
+    max-width: 180rpx;
+    margin-bottom: 16rpx;
+    font-size: 30rpx;
+
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .extra {
+    display: flex;
+    font-size: 20rpx;
+  }
+
+  .tips {
+    font-size: 22rpx;
+  }
+
+  .update {
+    padding: 3rpx 10rpx 1rpx;
+    color: rgba(255, 255, 255, 0.8);
+    border: 1rpx solid rgba(255, 255, 255, 0.8);
+    margin-right: 10rpx;
+    border-radius: 30rpx;
+  }
+
+  .settings {
+    position: absolute;
+    bottom: 0;
+    right: 40rpx;
+    font-size: 30rpx;
+    color: #fff;
+  }
+}
+
+/* 我的订单 */
+.orders {
+  position: relative;
+  z-index: 99;
+  padding: 30rpx;
+  margin: 50rpx 20rpx 0;
+  background-color: #fff;
+  border-radius: 10rpx;
+  box-shadow: 0 4rpx 6rpx rgba(240, 240, 240, 0.6);
+
+  .title {
+    height: 400rpx;
+    line-height: 40rpx;
+    font-size: 28rpx;
+    color: #1e1e1e;
+
+    .navigator {
+      font-size: 24rpx;
+      color: #939393;
+      float: right;
+    }
+  }
+
+  .section {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    padding: 40rpx 20rpx 10rpx;
+    .navigator,
+    .contact {
+      text-align: center;
+      font-size: 24rpx;
+      color: #333;
+      &::before {
+        display: block;
+        font-size: 60rpx;
+        color: #ff9545;
+      }
+      &::after {
+        border: none;
+      }
+    }
+    .contact {
+      padding: 0;
+      margin: 0;
+      border: 0;
+      background-color: transparent;
+      line-height: inherit;
+    }
+  }
 }
 </style>
